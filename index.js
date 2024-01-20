c35a87c1a208aca91ac90b9ddc73922cf05deeac
Charles Lim <charleslim0825@gmail.com>
2024-01-20 23:18:34 +0800

    Final Commit

--- index.js
+++ index.js
@@ -21,3 +21,21 @@ function openmenu() {
 function closemenu() {
   sidemenu.style.right = "-200px";
 }
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbx36utGDe1lGqslLEXsEhiyb2jkBtM24hMMqTkVt_AUZb4XQCF5bMtXtaa4d5wfQW1a/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
